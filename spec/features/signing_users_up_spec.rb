8b21a56fc682833e9745837ae490673a5cc7f011
Gray Smith <gray.smith00@gmail.com>
2016-03-24 17:14:45 +0000

    add user sign-in functionality and test

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -7,8 +7,8 @@ RSpec.feature "Users signup" do
     
     click_link "Sign up"
     fill_in "Email", with: "user@example.com"
-    fill_in "Passowrd", with: "password"
-    fill_in "Password Confirmation", with: "password"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     
     expect(page).to have_content("You have signed up successfully.")
